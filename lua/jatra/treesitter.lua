921315608d71568c2e2da5dd7577ef2a9fea7880
JatraMaya <jatramaya@mind-factory.local>
2022-08-25 21:10:27 +0800

    fix typo from nvim-treesitter.config into vim-treesitter.configs

--- lua/jatra/treesitter.lua
+++ lua/jatra/treesitter.lua
@@ -1,22 +1,30 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.config")
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 
 if not status_ok then
-    return
+	return
 end
 
-configs.setup{
-    ensure_installed = {"python"}, -- to list all the parser needed
-    sync_install = false, -- Install parsers synchronously for the list in ensure_installed
-    auto_install = true, -- Auto install missing parser when entering new buffer
-    ignore_install = {""}, -- To ignore a specific parser
-    highlight = {
-        enable = true, -- if set to false, will disable the whole extension
-        disable = {""}, -- list of parser to disable the highlight
+configs.setup({
+	ensure_installed = { "python" }, -- to list all the parser needed
+	sync_install = false, -- Install parsers synchronously for the list in ensure_installed
+	auto_install = true, -- Auto install missing parser when entering new buffer
+	ignore_install = { "" }, -- To ignore a specific parser
+	highlight = {
+		enable = true, -- if set to false, will disable the whole extension
+		disable = { "" }, -- list of parser to disable the highlight
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    }
-}
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+})
