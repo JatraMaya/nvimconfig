dacb2d466e0c9b0d9519ed99e6f2bf49de854407
JatraMaya <jatramaya@mind-factory.local>
2022-08-25 10:31:20 +0800

    improve basic window movement

--- lua/jatra/keymaps.lua
+++ lua/jatra/keymaps.lua
@@ -7,6 +7,12 @@ local k = vim.keymap.set
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
+-- movements
+k("n", "<C-l>", "<C-w>l", opts)
+k("n", "<C-h>", "<C-w>h", opts)
+k("n", "<C-j>", "<C-w>j", opts)
+k("n", "<C-k>", "<C-w>k", opts)
+
 k("i", "jj", "<Esc>", opts) -- Exit insert mode when press jk simultaneously
 k("n", "<leader>.", ":NvimTreeToggle<CR>", opts) -- map Ctrl+f to toggle NvimTree
 k("n", "<leader>/", ":CommentToggle<CR>", opts) -- map Ctrl+c to toggle comment
