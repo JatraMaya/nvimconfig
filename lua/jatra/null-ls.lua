22961914181ba5f682379d8c8d29889690eb25f3
JatraMaya <jatramaya@mind-factory.local>
2022-08-25 15:22:56 +0800

    add extra_args for prettier formatting

--- lua/jatra/null-ls.lua
+++ lua/jatra/null-ls.lua
@@ -1,13 +1,13 @@
 local null = require("null-ls")
 
 null.setup({
-    sources = {
-        null.builtins.formatting.stylua,
-        null.builtins.formatting.eslint,
-        null.builtins.formatting.black,
-        null.builtins.formatting.gofmt,
-        null.builtins.diagnostics.eslint,
-        -- null.builtins.diagnostics.flake8,
-        null.builtins.completion.spell,
-    },
+	sources = {
+		null.builtins.formatting.stylua,
+		null.builtins.formatting.prettier.with({extra_args = {"--no-semi", "--single-quote", "--jsx-single-quote"}}),
+		null.builtins.formatting.black,
+		null.builtins.formatting.gofmt,
+		null.builtins.diagnostics.eslint,
+		null.builtins.diagnostics.flake8,
+		null.builtins.completion.spell,
+	},
 })
