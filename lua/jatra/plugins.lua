dc655092b6ac49e26a8b2ab61f917ee1c02d45ce
JatraMaya <jatramaya@mind-factory.local>
2022-08-25 21:11:51 +0800

    add rainbow-ts plugins for multiple color curly braces

--- lua/jatra/plugins.lua
+++ lua/jatra/plugins.lua
@@ -1,97 +1,98 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use "EdenEast/nightfox.nvim" -- Add nightfox colorscheme varian
-  use "lunarvim/colorschemes" -- Plugin for adding extra colorschemes
-  use "terrortylor/nvim-comment" -- plugin for comment toggling
-  use "windwp/nvim-autopairs" -- vim-autopairs lua version
-  use "onsails/lspkind.nvim" -- Add pictogram vs-code look alike in neovim builtin-lsp
-  use "jose-elias-alvarez/null-ls.nvim" -- Add null-ls
-  use "simrat39/rust-tools.nvim" -- Add extra functionality for rust_analyzer lsp plugin
+	-- My plugins here
+	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+	use("EdenEast/nightfox.nvim") -- Add nightfox colorscheme varian
+	use("lunarvim/colorschemes") -- Plugin for adding extra colorschemes
+	use("terrortylor/nvim-comment") -- plugin for comment toggling
+	use("windwp/nvim-autopairs") -- vim-autopairs lua version
+	use("onsails/lspkind.nvim") -- Add pictogram vs-code look alike in neovim builtin-lsp
+	use("jose-elias-alvarez/null-ls.nvim") -- Add null-ls
+	use("simrat39/rust-tools.nvim") -- Add extra functionality for rust_analyzer lsp plugin
+	use("p00f/nvim-ts-rainbow") -- Add rainbow plugin for treesitter
 
-  -- Add nvim-tree
-  use {
-      "kyazdani42/nvim-tree.lua",
-      requires = {
-          "kyazdani42/nvim-web-devicons",
-      }
-  }
-  -- For Cmp (Completion) support
-  use {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
- }
+	-- Add nvim-tree
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+		},
+	})
+	-- For Cmp (Completion) support
+	use({
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+	})
 
-  -- For LSP support
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  }
+	-- For LSP support
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
 
-  -- Add treesitter plugin
-   use {
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate"
-  }
+	-- Add treesitter plugin
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
-  -- Add lua-line with devicons support
-  use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-}
+	-- Add lua-line with devicons support
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
