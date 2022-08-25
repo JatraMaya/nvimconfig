8ad8c7137da675d88e59cc3e76ec984f35a869b1
JatraMaya <jatramaya@mind-factory.local>
2022-08-25 10:22:03 +0800

    Fix keymap issue and add 2 miliseconds to the timeoutln

--- lua/jatra/keymaps.lua
+++ lua/jatra/keymaps.lua
@@ -3,14 +3,10 @@ local opts = { noremap = true, silent = true }
 local k = vim.keymap.set
 -- local term_opts = { silent = true }
 
+-- Map "," as leader key
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
--- Exit insert mode when press jk simultaneously
-k("i", "jk", "<Esc>", opts)
-
---map Ctrl+f to toggle NvimTree
-k("n", "<S-a>", ":NvimTreeToggle<CR>", opts)
-
--- map Ctrl+c to toggle comment
-k("n", "<C-c>", ":CommentToggle<CR>", opts)
+k("i", "jj", "<Esc>", opts) -- Exit insert mode when press jk simultaneously
+k("n", "<leader>.", ":NvimTreeToggle<CR>", opts) -- map Ctrl+f to toggle NvimTree
+k("n", "<leader>/", ":CommentToggle<CR>", opts) -- map Ctrl+c to toggle comment
