a9fdbe27aa2d3f96f21f1fb5b8cd1265a0400577
JatraMaya <jatramaya@mind-factory.local>
2022-08-26 09:07:32 +0800

    Change nvimtree config

--- lua/jatra/nvimtree.lua
+++ lua/jatra/nvimtree.lua
@@ -1,26 +1,26 @@
 local status_ok, tree = pcall(require, "nvim-tree")
 
 if not status_ok then
-    return
+	return
 end
 
 tree.setup({
-    sort_by = "case_sensitive",
-    view = {
-        adaptive_size = true,
-        },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
-    actions = {
-        open_file = {
-            quit_on_open = true,
-        },
-        remove_file = {
-            close_window = true,
-        },
-    },
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+	},
+	renderer = {
+		group_empty = false,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+		remove_file = {
+			close_window = true,
+		},
+	},
 })
