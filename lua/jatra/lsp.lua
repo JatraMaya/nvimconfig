5c19fb9a68a85a2a6509041a68483359e9aabd84
JatraMaya <jatramaya@mind-factory.local>
2022-10-23 17:12:23 +0800

    setup null-ls as default formatter if no other formatter provided

--- lua/jatra/lsp.lua
+++ lua/jatra/lsp.lua
@@ -8,16 +8,22 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            if client.name == "sumneko_lua" then
-                return client.name == "sumneko_lua"
-            elseif client.name == "tsserver" then
-                return client.name("tsserver")
-            end
-        end,
-        bufnr = bufnr,
-    })
+	vim.lsp.buf.format({
+		filter = function(client)
+			if client.name == "sumneko_lua" then
+				return client.name == "sumneko_lua"
+			elseif client.name == "tsserver" then
+				return client.name == "tsserver"
+			elseif client.name == "pyright" then
+				return client.name == "pyright"
+			elseif client.name == "rust_analyzer" then
+				return client.name == "rust_analyzer"
+			else
+				return client.name == "null-ls"
+			end
+		end,
+		bufnr = bufnr,
+	})
 end
 
 -- if you want to set up formatting on save, you can use this as a callback
@@ -25,41 +31,41 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 end
 
 local lsp_flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
+	-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
 }
 
 require("mason").setup()
@@ -68,26 +74,26 @@ local lspconfig = require("lspconfig")
 --
 mason_lsp.setup()
 mason_lsp.setup_handlers({
-    function(server_name)
-        lspconfig[server_name].setup({
-            on_attach = on_attach,
-            flags = lsp_flags,
-        })
-    end,
-    ["sumneko_lua"] = function()
-        lspconfig.sumneko_lua.setup({
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
-    end,
-    -- 	["rust_analyzer"] = function()
-    -- 		require("rust-tools").setup()
-    -- 	end,
-    -- 	["html"] = function() end,
+	function(server_name)
+		lspconfig[server_name].setup({
+			on_attach = on_attach,
+			flags = lsp_flags,
+		})
+	end,
+	["sumneko_lua"] = function()
+		lspconfig.sumneko_lua.setup({
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+	end,
+	-- 	["rust_analyzer"] = function()
+	-- 		require("rust-tools").setup()
+	-- 	end,
+	-- 	["html"] = function() end,
 })
